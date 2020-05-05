3744f7014ccaba68943c4b376d2fc38ab4d6c575
liber.roi <liber.roi@gmail.com>
2020-05-05 09:47:15 +0300

    Updates

---
+++ precache-manifest.dd948dedf6b7cc7ec841952ebec2f59a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6356293ba7833f0208a12f46e2206405",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a6bb00c77bc44b37cb1",
+    "url": "/static/css/main.f46c7e3c.chunk.css"
+  },
+  {
+    "revision": "8dbe09a08ed1307dbfce",
+    "url": "/static/js/2.c80ebb3f.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.c80ebb3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a6bb00c77bc44b37cb1",
+    "url": "/static/js/main.c6aa970c.chunk.js"
+  },
+  {
+    "revision": "3ac74ddc8fe9fa4acb87",
+    "url": "/static/js/runtime-main.e5fde76c.js"
+  },
+  {
+    "revision": "fd6df6c1b34506c36d80a800964edbdc",
+    "url": "/static/media/chip12.fd6df6c1.png"
+  },
+  {
+    "revision": "efbf11f2fdcd6cf53609abe953f0cb19",
+    "url": "/static/media/gold monkey 2.efbf11f2.png"
+  },
+  {
+    "revision": "c21354db93c249a0d91dfa1e5d1b3ddd",
+    "url": "/static/media/gold monkey.c21354db.png"
+  }
+]);
